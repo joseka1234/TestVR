bccfbccea746e4a7e65020a8bec5c5c3c9e8aa13
José Carlos <joseka1234@gmail.com>
2015-11-06 18:13:45 +0000

    Subiendo la practica

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control : MonoBehaviour {
+
+
+	public GameObject player;
+	float x;
+	float y;
+	float z;
+
+	// Use this for initialization
+	void Start () {
+		x = player.transform.position.x;
+		y = player.transform.position.y;
+		z = player.transform.position.z;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A))
+			x--;
+		if (Input.GetKey (KeyCode.D))
+			x++;
+		if (Input.GetKey (KeyCode.W))
+			z++;
+		if (Input.GetKey (KeyCode.S))
+			z--;
+
+		player.transform.position = new Vector3 (x,y,z);
+	}
+}
---
+++ Assets/Scripts/DistribucionCircular.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistribucionCircular : MonoBehaviour {
+
+	public GameObject cubo0;
+	public GameObject cubo1;
+	public GameObject cubo2;
+	public GameObject cubo3;
+	public GameObject cubo4;
+	public GameObject cubo5;
+
+		
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 aux = Vector3.zero;
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			aux = cubo0.transform.position;
+			cubo0.transform.position = cubo1.transform.position;
+			cubo1.transform.position = cubo2.transform.position;
+			cubo2.transform.position = cubo3.transform.position;
+			cubo3.transform.position = cubo4.transform.position;
+			cubo4.transform.position = cubo5.transform.position;
+			cubo5.transform.position = aux;
+
+
+		}
+	}
+}
